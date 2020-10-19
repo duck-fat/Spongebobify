aa726af5421406f71ee9658329108b69041166c1
elead1 <ericleadbetter@gmail.com>
2020-10-19 19:12:47 -0400

    Investigating a variable character flip chance.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from discord.ext import commands
 import random
 import os
+import asyncio
 
 INIT_CAP_CHANCE = 0.50  # initial probability to capitalize a letter
-
-BOT_ENABLED = True
-
+PER_CHAR_INCREASE = 0.15  # increase chance of case swap by this amount
+BOT_ENABLED = False
 
 bot = commands.Bot(command_prefix="!spong")
 
@@ -20,13 +20,18 @@ async def _convert(ctx, *phrase):
         roll = random.random()
         if roll > cap_chance:
             alt_cased.append(c.upper())
+            cap_chance = INIT_CAP_CHANCE
         else:
             alt_cased.append(c)
+            cap_chance -= PER_CHAR_INCREASE
     alt_cased = ''.join(alt_cased)
     print(alt_cased)
     if BOT_ENABLED:
         await ctx.send(alt_cased)
     return alt_cased
 
-token = os.environ.get("SPONGE_SECRET")
-bot.run(token)
+if BOT_ENABLED:
+    token = os.environ.get("SPONGE_SECRET")
+    bot.run(token)
+else:
+    asyncio.run(_convert(None, "this", "is", "the", "test", "phrase"))
