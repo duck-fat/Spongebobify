ec8f30ea94d462261f0213641e76bde77dcd5c86
elead1 <ericleadbetter@gmail.com>
2020-10-19 19:06:48 -0400

    Initial commit for image building.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Spongebobify
 Converts text to Spongebob chicken meme mocking case.
+
+imgflip account email: xnknhgtg@sharklasers.com
+user: spongebobifybot
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import os
 
 INIT_CAP_CHANCE = 0.50  # initial probability to capitalize a letter
+IMGFLIP_MEME_ID = 102156234  # meme ID for imgflip meme generator
 
 BOT_ENABLED = True
 
@@ -27,6 +28,22 @@ async def _convert(ctx, *phrase):
     if BOT_ENABLED:
         await ctx.send(alt_cased)
     return alt_cased
+
+
+@bot.command(name="epic")
+async def _generate_img(ctx, *text):
+    pass
+
+
+@bot.command(name="e help")
+async def _print_help(ctx):
+    help_text = [
+        "!spongify <text> to put your text into mocking case, a la the Mocking Spongebob meme.",
+        "!spongepic <text1>/<text2> to generate a Mocking Spongebob image macro.",
+        "!sponge help to generate this help text."
+    ]
+    ctx.send('<br>'.join(help_text))
+
 
 token = os.environ.get("SPONGE_SECRET")
 bot.run(token)
