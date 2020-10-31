59ff148971212aef46756eb4e0b87e3209e87e7a
Chris Mucciolo <chrismooch@gmail.com>
2020-10-30 20:45:14 -0400

    a leetle testing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import random
 import os
+import urllib.request
 
 INIT_CAP_CHANCE = 0.50  # initial probability to capitalize a letter
 
@@ -12,6 +13,7 @@ bot = commands.Bot(command_prefix="!spong")
 
 @bot.command(name="ify")
 async def _convert(ctx, *phrase):
+    print("the fuck")
     unaltered = ''.join(phrase) if len(phrase) == 1 else ' '.join(phrase)
     lowered = unaltered.lower()
     alt_cased = []
@@ -28,5 +30,18 @@ async def _convert(ctx, *phrase):
         await ctx.send(alt_cased)
     return alt_cased
 
+def internet_on():
+    try:
+        urllib.request.urlopen('http://216.58.192.142', timeout=1)
+        return True
+    except urllib.request.URLError as err:
+        return False
+
+if internet_on():
+    print("Got network!")
+else:
+    print("No network :( ")
+
 token = os.environ.get("SPONGE_SECRET")
+print(token)
 bot.run(token)
