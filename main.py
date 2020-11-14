d5b945b25d4ea7a821bb5b918cca525b1de434d0
Chris Mucciolo <chrismooch@gmail.com>
2020-11-14 15:11:20 -0500

    added precommit linting

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix="!spong")
 @bot.command(name="ify")
 async def _convert(ctx, *phrase):
     print("the fuck")
-    unaltered = ''.join(phrase) if len(phrase) == 1 else ' '.join(phrase)
+    unaltered = "".join(phrase) if len(phrase) == 1 else " ".join(phrase)
     lowered = unaltered.lower()
     alt_cased = []
     cap_chance = INIT_CAP_CHANCE
@@ -24,18 +24,20 @@ async def _convert(ctx, *phrase):
             alt_cased.append(c.upper())
         else:
             alt_cased.append(c)
-    alt_cased = ''.join(alt_cased)
+    alt_cased = "".join(alt_cased)
     print(alt_cased)
     if BOT_ENABLED:
         await ctx.send(alt_cased)
     return alt_cased
 
+
 def internet_on():
     try:
-        urllib.request.urlopen('http://216.58.192.142', timeout=1)
+        urllib.request.urlopen("http://216.58.192.142", timeout=1)
         return True
     except urllib.request.URLError as err:
         return False
+
 
 if internet_on():
     print("Got network!")
